558f8ca29e37abb7f25bce29accc954c6cb83153
Grant <dejong.grant@gmail.com>
2020-01-30 17:54:16 -0800

    Added filter removal options and configuration

--- DAS_junctions.R
+++ DAS_junctions.R
@@ -2,6 +2,8 @@
 
 # Add args to include: c("PSI", "PSI-SUPPA", "read counts") inputs
 # Edit --help docs
+# Modernize argument input; more python like?
+#   Benefit: easier to use; Drawback: not base R
 
 #####################################################################################
 ### Calculates differential splicing based on read counts of alt/cons transcripts ###
@@ -12,9 +14,16 @@
 rm( list= ls())
 args <- commandArgs(trailingOnly = TRUE)
 
+packages=c("magrittr","devtools","plyr","dplyr","tidyr","stats","car","brglm2")
+
 ## Libraries
 if(length(args) > 1) {
   # Some of these are redundant
+  
+  
+  invisible(lapply(packages, function(x){suppressPackageStartupMessages(suppressWarnings(library(noquote(x), quietly = T, character.only = T)))}))
+  
+  
   suppressWarnings(require(magrittr, quietly = T, warn.conflicts = FALSE))
   suppressWarnings(require(devtools, quietly = T, warn.conflicts = FALSE))
   suppressWarnings(require(plyr, quietly = T, warn.conflicts = FALSE))
@@ -38,13 +47,20 @@ if("--help" %in% args) {
       Differential Alternative Splicing Analysis
       
       Arguments:
-      --arg1=junction_counts.tsv  	- Table of constitutive vs alternative read counts per 
-        junction. Must be tsv.
+
+      args[1]=junction_counts.tsv  	  - Table of constitutive vs alternative read counts per 
+          junction. Must be tsv.
+
       OPTIONAL: 
-       sample = sampleinfo 		- string of condition information: uses this order your factors before DAS analysis.
-       plot =/plots/output/file/path  	- This will output a sample of 100 plots so choose
+
+       sample=sampleinfo 		          - string of condition information: uses this order your factors before DAS analysis.
+       n=N                            - Adjust the read depth filter (N = the sum of alt. reads support event across all samples)
+       filter=off                     - Use without a filter. May lead to downstream issues; use wisely.
+
+      WIP:
+        plot=/plots/output/file/path  - This will output a sample of 100 plots so choose
           the directory wisely!
-       plot                          	- Will output the plot by default to your other output folder
+        plot                          	- Will output the plot by default to your other output folder
         (WIP) SUPPA                   	- If you want to run this with SUPPA, include this string
        out=/output/file/path/        	- out=(insert filepath here)
         \n\n\n")
@@ -57,16 +73,28 @@ if(length(args) > 3) {
 
 ## Loading the data
 message("Loading the data")
-arsenal <- read.table(args[1], sep = '\t', header = T); arsenal$species<-NULL
 
-# Data.frame of 
+if(grepl("tsv", args[1])){
+  arsenal = read.table(args[1], sep = '\t', header = T); arsenal$species<-NULL
+} else if(grepl("csv", args[1])){
+  arsenal = read.csv(args[1], header = T); arsenal$species<-NULL
+} else {
+  message("Invalid splicing data format.")
+}
+
+
+# Adjust sampleinfo requirements.
 if(!any(grepl("sample", args))){
   # take column names as default
   message("No sampleinfo vector provided; using 'M' and 'I' as default conditions.")
   sampleinfo = colnames(arsenal)[grep("_alt", colnames(arsenal))] %>% gsub("[0-9].*$", "", .) %>% unique
   sampleinfo = factor(sampleinfo, levels = c(sampleinfo[grep("M", sampleinfo)], sampleinfo[grep("I", sampleinfo)]))
-} else if(any(grepl("sample", args))){
-sampleinfo = read.table(args[2], sep = '\t', header = T)
+} else if(any(grepl("^sample", args))){
+  # Deprecated; added for posterity.
+  sample_path = args[grep("^sample", args)] %>% gsub("sample=", "", .)
+  sampleinfo = read.table(sample_path, sep = '\t', header = T)
+} else if(any(grepl("sample", args[2]))){
+  sampleinfo = read.table(args[2], sep = '\t', header = T)
 }
 
 
@@ -93,10 +121,19 @@ BLR_format = function(x){
     mutate(sample_data=gsub("_nrm", "\\1",.$read_type)) %>% select(-read_type)
   df = cbind(alt,nrm)
   if(all(df[,1]==df[,4]) & all(df[,3]==df[,6])){
-  out = df[,unique(colnames(df))] %>%
-    mutate(condition_name = gsub("[0-9]+","",.$sample_data)) %>%
-    mutate(condition_name = factor(condition_name, levels = levels(sampleinfo))) %>%
-    mutate(condition = ifelse(condition_name == levels(factor(condition_name))[1], 1, 0)) %>%
+  out = df[,unique(colnames(df))]
+    # add sample information:
+    if(any(grepl("sample", args))){
+      # this step is horrible and grueling, need to find a better way.
+      out = merge(out, sampleinfo, by.x = "sample_data", by.y = "sample_info") %>% 
+        dplyr::rename(condition_name = condition) %>%
+        mutate(condition_name = factor(condition_name, levels = levels(sampleinfo$condition)))
+    } else {
+      out = mutate(out, condition_name = gsub("[0-9]+","",.$sample_data)) %>% 
+        mutate(condition_name = factor(condition_name, levels = levels(sampleinfo)))
+    }
+  # ???? I don't recall why this was done.
+  out = mutate(out, condition = ifelse(condition_name == levels(factor(condition_name))[1], 1, 0)) %>%
     mutate(condition = as.factor(condition))
   } else {
     message("There was a problem with the event merge!")
@@ -145,10 +182,23 @@ fold_change = function(x,n){
 ### Differential splicing ###
 #############################
 
-Juncs = filter_events(arsenal) %>% 
+# was a filter specified?
+if(any(grepl("n=", args))){
+  alt_filter = args[grep("^n=", args)] %>% gsub("n=", "", .) %>% as.numeric()
+} else { message("No specified alt. read depth cutoff; defaulting to sum(8) read minimum per event.") }
+
+
+if(!any(grepl("filter=off", args))){
+Juncs = filter_events(arsenal, alt_filter) %>% 
   # clean up df:
   unite(event_id, c("gene_name", "junct", "class", "unique_code")) %>%
   BLR_format(.)
+} else if(any(grepl("filter=off", args))){
+  Juncs = arsenal %>% 
+    # clean up df:
+    unite(event_id, c("gene_name", "junct", "class", "unique_code")) %>%
+    BLR_format(.)
+}
 
 message("Fitting models...")
 rPSI = BLR_fit(Juncs)
@@ -163,10 +213,10 @@ out = data.frame(BLR_test(rPSI))
 colnames(out) = paste0(n, "_padj")
 
 message("Saving results. If you provided none, check your R working directory.")
-dpsi_output = merge(out, LFC, by = "row.names") %>% rename(Event = Row.names)
+dpsi_output = merge(out, LFC, by = "row.names") %>% dplyr::rename(Event = Row.names)
 
 
-if(any(grepl("out=", args))){
+heaif(any(grepl("out=", args))){
   output=args[grep("out=", args)] %>% as.character(.) %>% gsub("out=(.*?)", "\\1", .)
   write.table(dpsi_output, file = paste0(output,"differential_splicing.txt"), row.names = T, col.names = T, quote = F, sep = "\t")
 }else{write.table(dpsi_output, row.names = T, col.names = T, sep = "\t", quote = F)}
